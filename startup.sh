94dd31d742c2d9a3727312d21a07d8f0470a9234
AnderMacedo <u20201c179@upc.edu.pe>
2025-07-01 12:58:29 -0500

    fix: set python version to 3.11.6 for Render

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/site/wwwroot
-python3.11 -m ensurepip --upgrade
-python3.11 -m pip install --upgrade pip
-python3.11 -m pip install -r requirements.txt
-python3.11 -m uvicorn main:app --host 0.0.0.0 --port 8000
